cd4a38c632c1c840fcd94b789449d9945ff2b2ac
Akshay <Akshaygrade05@gmail.com>
2022-02-25 12:21:24 +0530

    all error fixed

--- script.js
+++ script.js
@@ -1,197 +1,194 @@
-// first operation 
-// creating a model using + sign
-
-
-;
-let addbtn=document.querySelector('.add-btn');
-let addlfag=false;
+// button access
+let addbtn=document.querySelector(".add-btn");
+let addflag=false;
+ // remove ticket if we click
+ let removebtn=document.querySelector(".remove-btn");
 let removeflag=false;
-let removebtn=document.querySelector('.remove-btn');
-let modalcontainer=document.querySelector('.modal-container');
-let maincontainer=document.querySelector('.main-container');
-let textareacont=document.querySelector('.textarea-container');
+let lock="fa-lock";
 let lockopen="fa-lock-open";
-let lockclose="fa-lock";
-let toolboxcolors=document.querySelectorAll('.color');
-
-let ticketArr=[];
-
-
-
-
-// array of color
-let colors=['lightpink','lightblue','lightgreen','lightblack'];
-//  declairng black as default
-let modalPriorityColor=colors[colors.length-1];
-let allprioritycolor=document.querySelectorAll('.priority-color');
-
+let modalcont=document.querySelector(".modal-container");
+let maincont=document.querySelector('.main-container');
+let textareacont=document.querySelector(".textarea-container");
+let toolboxcolors=document.querySelectorAll(".color");
+let ticketsArr=[];
 
 for(let i=0;i<toolboxcolors.length;i++){
-    toolboxcolors[i].addEventListener("click",(e)=>{
-        let currtoolboxcolor=toolboxcolors[i].classList[0];
+    toolboxcolors[i].addEventListener('click',(e)=>{
+        let currenttoolboxcolor=toolboxcolors[i].classList[0];
 
-        let filterticket=ticketArr.filter((ticketObj,idx )=>{
-                return currtoolboxcolor===ticketObj.ticketcolor;
+
+        // filtering 
+        let filterticket=ticketsArr.filter((ticketobj,idx)=>{
+                return currenttoolboxcolor === ticketobj.ticketcolor;
         })
 
-        let alltickets=document.querySelectorAll(".ticket-container");
-        for(let i=0;i<alltickets.length;i++)
-        {
-            alltickets[i].remove();
+        // remove previous tickets
+        let allticketscont=document.querySelectorAll(".ticket-container");
+        for(let i=0;i<allticketscont.length;i++){
+            allticketscont[i].remove();
         }
-         filterticket.forEach((ticketObj)=>{
-                createTicket(ticketObj.ticketcolor,ticketObj.tickettask,ticketObj.ticketid)
-            })
-    })
-    toolboxcolors[i].addEventListener('dblclick',(e)=>{
-        let alltickets=document.querySelectorAll(".ticket-container");
-        for(let i=0;i<alltickets.length;i++)
-        {
-            alltickets[i].remove();
-        }
-        ticketArr.forEach((ticketObj,idx)=>{
-            createTicket(ticketObj.ticketcolor,ticketObj.tickettask,ticketObj.ticketid);
+
+        // display new filtertickets
+        filterticket.forEach((ticketobj,idx)=>{
+            // again click we genereate duplicay  of same ticket 
+            createTicket(ticketobj.ticketcolor,ticketobj.tickettask,ticketobj.ticketID);
         })
-        
     })
+   toolboxcolors[i].addEventListener("dblclick",(e)=>{
+
+ let allticketscont=document.querySelectorAll(".ticket-container");
+        for(let i=0;i<allticketscont.length;i++){
+            allticketscont[i].remove();
+        }
+        ticketsArr.forEach((ticketobj,idx)=>{
+            createTicket(ticketobj.ticketcolor,ticketobj.tickettask,ticketobj.ticketID);
+        })
+
+    })
+    
+
 }
 
-// listner for modal priority coloring and for clicking every color
-// foreach loop becuase for everycolor so add click functionality and border for them
-allprioritycolor.forEach((colorELem,idx)=>{
-colorELem.addEventListener("click",(e)=>{
-    // removing border from every color
-    // remove border if their is any border in any color but display  border where we click later 
-    allprioritycolor.forEach((pricoloEle,idx)=>{
-        pricoloEle.classList.remove("border");
 
+
+let colors=["lightpink","lightblue","lightgreen","lightblack"];
+// defalut color for modal;
+let modalpriocolor=colors[colors.length-1];
+
+let allpriocolor=document.querySelectorAll(".priority-color");
+
+// listner for modal priority listening 
+
+allpriocolor.forEach((colorele,idx)=>{
+    colorele.addEventListener('click',(e)=>{
+        // removing border
+        allpriocolor.forEach((priocolor)=>{
+                priocolor.classList.remove("border");
+            
+        })
+        colorele.classList.add("border");
+        modalpriocolor=colorele.classList[0];
     })
-    // specific color where click happpen we add click target which user click we use border
-    colorELem.classList.add("border");
-    modalPriorityColor=colorELem.classList[0];
 })
-})
-
 addbtn.addEventListener('click',(e)=>{
+    // display modal
+
+
     
+    // addflag =true --display modal 
 
-    // addflag=true-> model display
+    // add flag - false - remove modal
 
-    // addflag=flase->model none
+    addflag=!addflag;
+    // state change for every click;
 
-    addlfag=!addlfag;
-    if(addlfag){
-        modalcontainer.style.display="flex";
-    
-    }else{
-        modalcontainer.style.display="none";
-    }
-    
-})
-
-removebtn.addEventListener("click",(e)=>{
-    removeflag=!removeflag;
-})
-// we keyboard functionality if we hold shift the textarea will be quit and create the ticket  
-modalcontainer.addEventListener('keydown',(e)=>{
-    // tells us what key we have pressed 
-    let  mykey=e.key;
-    // if we press shift then create the ticket 
-    if(mykey==="Shift"){
-        // passing the modalpriority because if we click the lighpink so value will be passed here to so we get first value 
-        createTicket(modalPriorityColor,textareacont.value);
-        // here we diplay  none after press down the shift key and modal area will be down 
-        setmmodaldefault();
-        // we put value attribute then it set empty in textarea
-        textareacont.value='';
-    }
-})
-
-// it will create a ticket and append it to main container where ticket are generetating
-// passing ticket id and priority color inject in ticket so pass as arguments
-function createTicket(ticketcolor,tickettask,ticketid){
-    let ID= ticketid || shortid();
-    let ticketcont=document.createElement('div');
-    ticketcont.setAttribute('class','ticket-container');
-    ticketcont.innerHTML=
-    
-    `<div class="ticket-color ${ticketcolor}"></div>
-    <div class="ticket-id">#${ticketid}</div>
-    <div class="task-area">${tickettask}</div>
-    
-    <div class="lock-cont">
-    <i class="fa-solid fa-lock"></i>
-    </div>`;
-    
-    maincontainer.appendChild(ticketcont);
-
-    // create obj ticket and add to array
-
-    if(!ticketid){
-        ticketArr.push({ticketcolor,tickettask,ticketid:ID });
-        console.log(ticketArr);
-    }
-        
    
-    removehandel(ticketcont);
-    lockhandel(ticketcont);
-    colorhandel(ticketcont);
+    
 
+    
+    if(addflag=true){
+
+        modalcont.style.display="flex";
+    }
+    else{
+        modalcont.style.display="none";
+    }
+})
+removebtn.addEventListener('click',(e)=>{
+    removeflag=!removeflag; //toggle remove btn
+
+})
+modalcont.addEventListener("keydown",(e)=>{
+    let key=e.key; 
+    if(key==="Shift"){
+        createTicket(modalpriocolor,textareacont.value);
+        addflag=false;
+         setmodaldefault();
+       
+        
+        
+    }
+})
+function createTicket(ticketcolor,tickettask,ticketID){
+    let id=ticketID || shortid();
+    let ticketcont=document.createElement('div');
+    ticketcont.setAttribute("class","ticket-container");
+    ticketcont.innerHTML=`
+    <div class ="ticket-color ${ticketcolor}"></div>
+    <div class="ticket-id">#${id}</div>
+    <div class="task-area">${tickettask}</div>
+    <div class="lock-cont">
+    <i class="fa-solid fa-lock"></i></div>`;
+    // create objj of ticket and add to array
+    // push when no ticket id 
+    if(!ticketID){
+    ticketsArr.push({ticketcolor,tickettask,ticketID:id})};
+    maincont.appendChild(ticketcont);
+    handelremoval(ticketcont);
+    handellock(ticketcont);
+    handelcolor(ticketcont);
 }
-function removehandel(ticket){
+function handelremoval(ticket){
+    // remove flag is true  ->remove
     ticket.addEventListener("click",(e)=>{
         if(removeflag){
             ticket.remove();
         }
     })
-
-
-}
-function lockhandel(ticket){
-    // /access of lock -cont
-
-    let lockcont=ticket.querySelector('.lock-cont');
-    let lockpic=lockcont.children[0];
-    let taskArea=ticket.querySelector('.task-area')
-    lockpic.addEventListener('click',(e)=>{
-        if(lockpic.classList.contains(lockclose)){
-            lockpic.classList.remove(lockclose);
-            lockpic.classList.add(lockopen);
-            taskArea.setAttribute("contenteditable",true);
-        }
-        else{
-            lockpic.classList.remove(lockopen);
-            lockpic.classList.add(lockclose);
-            taskArea.setAttribute("contenteditable",false);
-
-        }
-    })
-
-}
-function colorhandel(ticket){
-    let ticketcol=ticket.querySelector('.ticket-color');
-    ticketcol.addEventListener('click',(e)=>{
-        let currentcoltick=ticketcol.classList[1];
-        let currentcolidx=colors.findIndex((color)=>{
-            return currentcoltick === color;
-        })
-        currentcolidx++;
-        let nextcoloridx=currentcolidx%colors.length;
-        let nextcolor=colors[nextcoloridx];
-
-        ticketcol.classList.remove(currentcoltick);
-        ticketcol.classList.add(nextcolor);
-    })
    
-}
-function setmmodaldefault(ticket){
 
-    modalcontainer.style.display='none';
-    addflag=!addlfag;
-    modalPriorityColor=colors[colors.length-1];
-    allprioritycolor.forEach((pricoloEle,idx)=>{
-        pricoloEle.classList.remove("border");
+}
+function handellock(ticket){
+    let ticketlockele=ticket.querySelector(".lock-cont");
+    let ticketlock=ticketlockele.children[0];
+    let tickettaskarea=ticket.querySelector('.task-area');
+    ticketlock.addEventListener('click',(e)=>{
+        if(ticketlock.classList.contains(lock)){
+            ticketlock.classList.remove(lock);
+            ticketlock.classList.add(lockopen);
+            // edit text using content editable attribute 
+            tickettaskarea.setAttribute("contenteditable","true");
+
+            
+
+        }else{
+            ticketlock.classList.add(lock);
+            ticketlock.classList.remove(lockopen);
+            tickettaskarea.setAttribute("contenteditable","false");
+
+        }
 
     })
-    allprioritycolor[allprioritycolor.length-1].classList.add('border');
+}
+function handelcolor(ticket){
+    let ticektcolorss=ticket.querySelector(".ticket-color");
+    ticektcolorss.addEventListener('click',(e)=>{
+    let currentticketcolor=ticektcolorss.classList[1];
+    // find current color
+    // acccesing index
+    let currentcolorindex=colors.findIndex((color)=>{
+        return currentticketcolor===color;
+    })
+    currentcolorindex++;
+    // for making it again 0  because we dont have 4 index for color
+    let newcoloridx=currentcolorindex%colors.length;
+    let newcolor=colors[newcoloridx];
+    ticektcolorss.classList.remove(currentticketcolor);
+    ticektcolorss.classList.add(newcolor);
+    
+
+    })
+}
+function setmodaldefault(ticket){
+    // remove modal
+    modalcont.style.display='none';
+    
+    // textareacont.innerText="";  value should be empty
+    textareacont.value="";
+    modalpriocolor=colors[colors.length-1];
+    allpriocolor.forEach((priocolor)=>{
+        priocolor.classList.remove("border");
+    })
+    allpriocolor[allpriocolor.length-1].classList.add("border");
+    
 }
